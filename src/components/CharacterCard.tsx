408ae41ed8b2d4879fc37f16fc0f2f2233038ec4
Jordan <jche681@aucklanduni.ac.nz>
2018-09-16 23:50:45 +1200

    Code fix up

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -3,25 +3,12 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-
-// import { CircularProgress } from '@material-ui/core';
-
-// import * as equal from 'fast-deep-equal';
 import * as React from 'react';
 
-import './GameCard.css';
 
-interface IState {
-    error: string
-    character: any
-    loading: boolean
-}
-
-export default class CharacterCard extends React.Component<{ characterId: any, characterName: any }, IState>{
+export default class CharacterCard extends React.Component<{ characterId: any, characterName: any }>{
     public render() {
         const { characterId, characterName } = this.props
-        // tslint:disable-next-line:no-console
-        console.log(characterId);
         return (
             <Grid item={true} sm={6} md={4} lg={3}>
                 <Card className="card" style={{ height: 'auto', overflow: 'auto' }}>
@@ -36,18 +23,6 @@ export default class CharacterCard extends React.Component<{ characterId: any, c
                     </CardContent>
                 </Card>
             </Grid>
-            // <div className="character">
-            //     <button
-            //         type="button"
-            //         className="character_sprite"
-            //         style={{
-            //             backgroundImage: `url(${`https://rickandmortyapi.com/api/character/avatar/${
-            //                 characterId
-            //                 }.jpeg`})`
-            //         }}
-            //     />
-            //     <p className="character_name">{characterName}</p>
-            // </div>
         )
 
     }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,10 @@
 import { Input } from '@material-ui/core/';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
 
 import * as React from 'react';
 import CharacterCard from './CharacterCard';
 
 interface IState {
-    error: any,
     characters: any,
     loading: boolean
 }
@@ -16,22 +14,13 @@ export default class SearchBar extends React.Component<{}, IState> {
     constructor(props: any) {
         super(props)
         this.state = {
-            error: "",
             characters: [],
             loading: false
         }
-        this.updateInputValue = this.updateInputValue.bind(this);
+        this.updateInput = this.updateInput.bind(this);
     }
 
     public render() {
-        // (this.state.characters === null || typeof this.state.characters === 'undefined') ? {const newCharacters = this.state.characters.map((character: any) => {
-        //     return (
-        //       <li className="pokemons__item" key={character.id}>
-        //         <CharacterCard character={character}/>
-        //       </li>
-        //     )
-        // })} : null
-
         return (
             <div>
                 <Grid container={true} xs={12} justify={'center'}>
@@ -45,31 +34,19 @@ export default class SearchBar extends React.Component<{}, IState> {
                                     'aria-label': 'Description',
                                 }}
                                 fullWidth={true}
-                                onKeyPress={this.updateInputValue}
+                                onKeyPress={this.updateInput}
                             />
-                            <Grid container={true} justify={'center'} xs={12}>
-                                <Grid item={true}>
-                                    {this.state.loading ? <CircularProgress thickness={3} /> : (
-                                        this.state.error !== "" ? <p>An error occured, please try again later.</p> : (
-                                            (this.state.characters.characters === null || typeof this.state.characters.characters === 'undefined') ? <p>Enter a character to want to find</p> :
-                                                ""
-                                        )
-                                    )}
-                                </Grid>
-                            </Grid>
                         </Grid>
                     </Grid>
 
                     <Grid item={true} xs={12}>
                         {this.state.loading ? "" : (
-                            this.state.error !== "" ? "" : (
                                 (this.state.characters.characters === null || typeof this.state.characters.characters === 'undefined') ? "" :
                                     <Grid container={true} spacing={40}>
                                         {this.state.characters.characters.map((character: any) => (
                                             <CharacterCard key={character.index} characterId={character.id} characterName={character.name}/>
                                         ))}
                                     </Grid>
-                            )
                         )}
                     </Grid>
                 </Grid>
@@ -77,15 +54,10 @@ export default class SearchBar extends React.Component<{}, IState> {
         );
     }
 
-    private updateInputValue(evt: any) {
+    private updateInput(evt: any) {
         if (evt.key === 'Enter') {
             this.setState({ loading: true });
             this.getCharacters(evt.target.value);
-
-            // tslint:disable-next-line:no-console
-            console.log(this.state.characters);
-            // tslint:disable-next-line:no-console
-            console.log(typeof(this.state.characters));
         }
     }
 
@@ -103,12 +75,9 @@ export default class SearchBar extends React.Component<{}, IState> {
                 return character;
             })
             this.setState({
-                error: "",
                 characters: {characters},
                 loading: false,
             });
-            // tslint:disable-next-line:no-console
-            console.log(characters);
             return characters;
             
         })
